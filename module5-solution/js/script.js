de0d2df04ae86c3b4a4d59a101d6582ef89034c7
arunas <arunas.mdk@gmail.com>
2020-12-05 22:48:00 +0200

    5-solution

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -99,7 +99,7 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
       // console.log(categories);
-      chooseRandomCategory();
+      chooseRandomCategory(categories);
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
