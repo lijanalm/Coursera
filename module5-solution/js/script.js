c32a332ee58926c3d80538567065b0bd98bac884
arunas <arunas.mdk@gmail.com>
2020-12-05 22:44:36 +0200

    5-solution

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -98,7 +98,8 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-      console.log(categories);
+      // console.log(categories);
+      chooseRandomCategory;
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
