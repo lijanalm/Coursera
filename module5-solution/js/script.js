37884edca4849b24242150db77e4e0470907b09d
arunas <arunas.mdk@gmail.com>
2020-12-05 23:56:55 +0200

    5-solution_v3

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-      var randomCategoryShortName = "" + chosenCategoryShortName;
+      var randomCategoryShortName = "'" + chosenCategoryShortName + "'";
       console.log(randomCategoryShortName);
  
       var html = homeHtmlUrl;
